9941e832fed7b67e7f2b7dbda0e59c3ef8a52f5d
Nick Khitrov <th1sistom@yandex.ru>
2020-10-09 10:55:18 +0300

    Add the "use-strict" Directive and refactor the code, split the code by function

--- src/assets/blocks/rate-button/rate-button.js
+++ src/assets/blocks/rate-button/rate-button.js
@@ -1,14 +1,43 @@
-let rating = document.querySelectorAll('.rating')
+'use strict';
 
-rating.forEach(item => { 
-  item.addEventListener('click', e => {
-    if(!e.target.matches('i')) return
-    // if(e.target.tagName !== 'I') return
-    for(let i = 0; i < item.children.length; i++){
-      item.children[i].children[0].innerHTML = 'star_border'
-    }
-    for(let i = 0; i < e.target.parentElement.getAttribute('data-rating'); i++){
-      item.children[i].children[0].innerHTML = 'grade'
-    }
-  })
+const rating = document.querySelectorAll('.js-rating');
+
+rating.forEach(item => {
+  item.addEventListener('click', putRatingHandler);
 })
+
+function putRatingHandler(star) {
+  if(!star.target.matches('span')) return;
+  let starList = star.target.parentElement.parentElement.children;
+  let starItem = star.target.parentElement;
+  let starTarget = star.target;
+
+  if (revote(starTarget)) {
+    starTarget.removeAttribute('data-rating');
+    clearRating();
+  } else {
+    clearRating();
+    setRating(starItem);
+  }
+
+  function setRating(starParent) {
+    starTarget.setAttribute('data-rating', 'check');
+    for (let i = 0; i <= starParent.getAttribute('data-rating'); i++) {
+      starList[i].children[0].innerHTML = 'grade';
+    }
+  }
+
+  function clearRating() {
+    for (let i = 0; i < starList.length; i++) {
+      starList[i].children[0].removeAttribute('data-rating');
+      starList[i].children[0].innerHTML = 'star_border';
+    }
+  }
+
+  function revote(star) {
+    if (star.hasAttribute('data-rating')) {
+      star.removeAttribute('data-rating');
+      return true;
+    }
+  }
+}
