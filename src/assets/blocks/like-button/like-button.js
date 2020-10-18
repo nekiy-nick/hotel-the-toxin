8dbe053448efddf6f513a492cd4e08c6868ccd60
Nick Khitrov <th1sistom@yandex.ru>
2020-10-18 13:12:38 +0300

    Add the directive 'use strict' and a little refactoring of functional

--- src/assets/blocks/like-button/like-button.js
+++ src/assets/blocks/like-button/like-button.js
@@ -1,12 +1,12 @@
-let likeBtn = document.querySelectorAll('.like-button')
-for(let i = 0; i < likeBtn.length; i++){
-  likeBtn[i].addEventListener('click', function(e){
-    this.classList.toggle('like-button_active')
-    // смена счётчика лайка
-    if (this.classList.contains('like-button_active')) {
-      this.textContent = `${Number(this.textContent) + 1}`;
-    } else {
-      this.textContent = `${Number(this.textContent) - 1}`;
-    }
-  })
-}
+'use strict';
+
+let likeBtn = document.querySelectorAll('.js-like-button');
+
+likeBtn.forEach(btn => btn.addEventListener('click', e => {
+  btn.classList.toggle('like-button_active');
+  if (btn.classList.contains('like-button_active')) {
+    btn.textContent = `${Number(btn.textContent) + 1}`;
+  } else {
+    btn.textContent = `${Number(btn.textContent) - 1}`;
+  }
+}));
