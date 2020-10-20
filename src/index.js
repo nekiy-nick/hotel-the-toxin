19b6d42234f5dadc7395838a262f86ab443fea5d
Nick Khitrov <th1sistom@yandex.ru>
2020-10-20 14:59:31 +0300

    Import component scripts and juery plugins to the project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 // сюда импортируются локальные файлы и библиотеки
+import '../node_modules/jquery.maskedinput/src/jquery.maskedinput'
+import '../node_modules/ion-rangeslider/js/ion.rangeSlider'
+import '../node_modules/ion-rangeslider/css/ion.rangeSlider.min.css'
+import '../node_modules/paginationjs/dist/pagination.min'
+import '../node_modules/paginationjs/dist/pagination.css'
+
 import '@fortawesome/fontawesome-free/js/all.js'
 import './assets/scss/main.scss'
+
+import './assets/pages/colors-and-types/colors-and-types'
+
+import './assets/blocks/like-button/like-button'
+
+import './assets/blocks/rate-button/rate-button'
+
+import './assets/blocks/expandable-checkbox-list/expandable-checkbox-list'
+
+import './assets/blocks/feedback/feedback'
+// импорт логики блока dropdown
+import './assets/blocks/dropdown/dropdown'
+// импорт инпута с маской даты
+import './assets/blocks/input-text-field/input-text-field'
+
+import './assets/blocks/range-slider/range-slider'
+import './assets/blocks/pagination/pagination'
